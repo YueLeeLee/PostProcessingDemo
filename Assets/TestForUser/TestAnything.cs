3d922d6bac7479f9596a0f9c707227337e7bd367
YueLeeLee <49544195+YueLeeLee@users.noreply.github.com>
2019-06-12 10:54:19 +0800

    Add files via upload

---
+++ Assets/TestForUser/TestAnything.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestAnything : MonoBehaviour
+{
+    public int testNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+        //if(transform.GetSiblingIndex()==0)
+        Debug.Log("OnValidate " + transform.GetSiblingIndex(), this);
+    }
+}
